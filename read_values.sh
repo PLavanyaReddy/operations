30be46d2a4358ece22e836d4352188c13ec26a6f
unknown <lavanyareddy19994@gmail.com>
2020-07-13 21:50:52 +0530

    modified

--- read_values.sh
+++ read_values.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 function operation4(){
 	opr4=$(( ($1%$2)+$3 ))
+	dict["opr4"]=$opr4
+}
 function operation3(){
 	opr3=$(( $3+($1/$2) ))
+	dict["opr3"]=$opr3
+}
 function operation1(){
         opr1=$(( $1+($2*$3) ))
+	dict["opr1"]=$opr1
 }
 function operation2(){
 	opr2=$(( ($1*$2)+$3 ))
+	dict["opr2"]=$opr2
 }
 read -p "Enter first value" num1
 read -p "Enter second value" num2
@@ -16,8 +22,8 @@ operation4 $num1 $num2 $num3
 echo "The result of a%b+c is "$opr4
 operation3 $num1 $num2 $num3
 echo "The result of c+a/b is "$opr3
-operation1 $num $num2 $num3
+operation1 $num1 $num2 $num3
 echo "The result of a+b*c is "$opr1
 operation2 $num1 $num2 $num3
 echo "The result of a*b+c is "$opr2
-
+echo ${dict[@]}
