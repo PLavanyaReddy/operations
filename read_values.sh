ed3fc5b59a53a1ed2bdeb150b8dcc257cfeea73f
unknown <lavanyareddy19994@gmail.com>
2020-07-13 17:01:29 +0530

    performed operation3

--- read_values.sh
+++ read_values.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+function operation3(){
+	opr3=$(( $3+($1/$2) ))
+}
 read -p "Enter first value" num1
 read -p "Enter second value" num2
 read -p "Enter third value" num3
+operation3 $num1 $num2 $num3
+echo "The result of c+a/b is "$opr3
