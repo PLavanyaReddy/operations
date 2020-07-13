30eafb89962360cefdadf9b5da948e9aea5e25b3
unknown <lavanyareddy19994@gmail.com>
2020-07-13 20:42:54 +0530

    modified

--- read_values.sh
+++ read_values.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-function operation1(){
-	opr1=$(( $1+($2*$3) ))
-}
 function operation2(){
 	opr2=$(( ($1*$2)+$3 ))
 }
 read -p "Enter first value" num1
 read -p "Enter second value" num2
 read -p "Enter third value" num3
-operation1 $num1 $num2 $num3
-echo "The result of a+b*c is "$opr1
-operation1 $num1 $num2 $num3
-echo "The result of a+b*c is "$opr2
+operation2 $num1 $num2 $num3
+echo "The result of a*b+c is "$opr2
 
